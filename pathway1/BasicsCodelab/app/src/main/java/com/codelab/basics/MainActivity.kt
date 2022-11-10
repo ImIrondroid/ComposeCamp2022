26675b2110c0e7e4b23b802e562add916f264cf7
iron <satls34@naver.com>
2022-11-10 22:38:30 +0900

    상태 유지

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -96,7 +97,7 @@ private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember {
+    val expanded = rememberSaveable {
         mutableStateOf(false)
     }
     val extraPadding = if(expanded.value) 48.dp else 0.dp
